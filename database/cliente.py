a2271c3136647dda79ab6f8bf2860418816ec0fe
Kyoya <jansonquiel@gmail.com>
2024-04-07 14:54:23 -0300

    first commit

---
+++ database/cliente.py
@@ -0,0 +1,8 @@
+CLIENTES = [
+    {"id": 1, "nome": "Guilherme", "email": "teste@teste.com" },
+    {"id": 2, "nome": "Jorge", "email": "teste@teste.com" },
+    {"id": 3, "nome": "Maria", "email": "teste@teste.com" },
+    {"id": 4, "nome": "Felipe", "email": "teste@teste.com" },
+    {"id": 5, "nome": "Joao", "email": "teste@teste.com" },
+    {"id": 6, "nome": "Jesus", "email": "teste@teste.com" },
+]
